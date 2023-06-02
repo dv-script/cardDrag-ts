6b1e7b4404f36b5834acebe832df24353feff5e0
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2023-06-02 13:31:47 -0300

    fix: bug solucition menu zone

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,18 @@
 import { Rnd } from 'react-rnd'
 import Menu from '../../components/MenuHeader'
 import MenuMain from '../../components/MenuMain'
+import { MainContainer } from './style'
 
 export default function Home() {
   return (
     <>
       <Menu></Menu>
-      <Rnd default={{ x: 5, y: 80, width: 0, height: 10 }}>
-        <MenuMain />
-      </Rnd>
+
+      <MainContainer>
+        <Rnd default={{ x: 5, y: 100, width: 60, height: 0 }} bounds={'parent'}>
+          <MenuMain />
+        </Rnd>
+      </MainContainer>
     </>
   )
 }
