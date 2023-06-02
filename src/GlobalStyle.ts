a0c019ea943bff0a7f2ec188a2cb456761c04906
Alex Oliveira <alexoliveira7x@gmail.com>
2023-06-02 20:07:29 +0100

    fix: global style

--- src/GlobalStyle.ts
+++ src/GlobalStyle.ts
@@ -12,10 +12,11 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
     background: rgb(255,255,255);
     background: radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(119,40,193,1) 0%, rgba(109,52,189,1) 12%, rgba(37,136,162,1) 100%);
+    font-family: "Poppins";
+  }
 
-    * {
-      font-family: "Poppins";
-    }
+  button {
+    font-family: inherit;
   }
 
   p {
